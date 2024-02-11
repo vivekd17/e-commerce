d6b2d764d9930b22161215f981338fa35ef9f11d
Vivek <vivekvalvid17@gmail.com>
2024-02-11 17:17:42 +0530

    new data inserted

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from '../Assets/img1.jpg'
+import p2_img from '../Assets/img10.jpg'
+import p3_img from '../Assets/img2.jpg'
+import p4_img from '../Assets/img3.jpg'
+
+let data_product = [
+    {
+        id:1,
+        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image:p1_img,
+        new_price:50.50,
+        old_price: 120,
+    },
+    {
+        id:1,
+        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image:p2_img,
+        new_price:50.50,
+        old_price: 120,
+    },
+    {
+        id:1,
+        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image:p3_img,
+        new_price:50.50,
+        old_price: 120,
+    },
+    {
+        id:1,
+        name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image:p4_img,
+        new_price:50.50,
+        old_price: 120,
+    },
+
+]
+export default data_product;
