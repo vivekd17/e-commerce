728490e93362ae9c9750b56a24abc81bb977287f
Vivek <vivekvalvid17@gmail.com>
2024-02-18 13:37:17 +0530

    route chng

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from '../src/Components/Footer/Footer'
+import man_banner from '../src/Components/Assets/man_banner.jpg'
+import woman_banner from '../src/Components/Assets/women_banner.jpg'
+import kids_banner from '../src/Components/Assets/Kids_banner.jpg'
 
 function App() {
   return (
@@ -16,9 +19,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="mens" />} />
-          <Route path='/women' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory category="kids" />} />
+          {/* <Route path='/mens' element={<ShopCategory category="mens" />} /> */}
+          <Route path='/men' element={<ShopCategory banner={man_banner} category="men" />} />
+          <Route path='/women' element={<ShopCategory banner={woman_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kids" />} />
           <Route path='/products/:productId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </div>
         <ul className="nav-menu">
           <li onClick={() => setMenu("shop")}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" && <hr />}</li>
-          <li onClick={() => setMenu("mens")}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" && <hr />}</li>
+          <li onClick={() => setMenu("men")}><Link style={{ textDecoration: 'none' }} to='/men'>Men</Link>{menu === "men" && <hr />}</li>
           <li onClick={() => setMenu("women")}><Link style={{ textDecoration: 'none' }} to='/women'>Women</Link>{menu === "women" && <hr />}</li>
           <li onClick={() => setMenu("kids")}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu === "kids" && <hr />}</li>
         </ul>
