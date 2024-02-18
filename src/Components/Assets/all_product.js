59b1974e56d3d31652959f4173460090019766b0
Vivek <vivekvalvid17@gmail.com>
2024-02-18 13:36:07 +0530

    add imgs and rm unwanted

--- src/Components/Assets/all_product.js
+++ src/Components/Assets/all_product.js
@@ -1,222 +1,343 @@
-// import p1_img from "./Kids/img_1.jpg"
-// import p2_img from "./Kids/img_2.jpg"
-// import p3_img from "./Kids/img_3.jpg"
-// import p4_img from "./Kids/img_4.jpg"
-// import p5_img from "./men/pg1.webp"
-// import p6_img from "./men/pg2.jpg"
-// import p7_img from "./men/pg3.jpg"
-// import p8_img from "./men/pg4.jpg"
-// import p9_img from "./men/pg5.jpg"
-// import p10_img from "./men/pg6.jpg"
-// import p11_img from "./men/pg8.jpg"
-// import p12_img from "./men/pg9.jpg"
-// import p13_img from "./men/pg10.jpg"
-// import p14_img from "./men/pg11.webp"
-// import p15_img from "./women/img1.jpg"
-// import p16_img from "./women/img4.jpg"
-// import p17_img from "./women/img2.jpg"
-// import p18_img from "./women/img3.jpg"
-// import p19_img from "./women/img5.jpg"
-// import p20_img from "./women/img6.jpg"
-// import p21_img from "./women/img7.jpg"
-// import p22_img from "./women/img8.jpg"
-// import p23_img from "./women/img9.jpg"
-// import p24_img from "./women/img10.jpg"
-// //36 products
+// Kids Section
+import p1_img from "./Kids/img_1.jpg";
+import p2_img from "./Kids/img_2.jpg";
+import p3_img from "./Kids/img_3.jpg";
+import p4_img from "./Kids/img_4.jpg";
 
+// Men Section
+import p5_img from "./men/pg1.jpg";
+import p6_img from "./men/pg2.jpg";
+import p7_img from "./men/pg3.jpg";
+import p8_img from "./men/pg4.jpg";
+import p9_img from "./men/pg5.jpg";
+import p10_img from "./men/pg6.jpg";
+import p101_img from "./men/pg7.jpg";
+import p11_img from "./men/pg8.jpg";
+import p12_img from "./men/pg9.jpg";
+import p13_img from "./men/pg10.jpg";
+import p14_img from "./men/pg11.jpg";
+import p15_img from "./men/pg12.jpg";
+import p16_img from "./men/pg13.jpg";
+import p17_img from "./men/pg14.jpg";
+import p18_img from "./men/pg15.jpg";
+import p19_img from "./men/pg16.jpg";
 
-// let all_product = [
-//     {
-//         id: 1,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "kids",
-//         image: p1_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 2,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "kids",
-//         image: p2_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 3,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "kids",
-//         image: p3_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 4,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "kids",
-//         image: p4_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 5,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p5_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 6,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p6_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 7,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p7_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 8,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p8_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 9,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p9_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 10,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p10_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 11,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p11_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 12,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p12_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 13,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p13_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 14,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "men",
-//         image: p14_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 15,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p15_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 16,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p16_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 17,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p17_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 18,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p18_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 19,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p19_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 20,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p20_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 21,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p21_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 22,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p22_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 23,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p23_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-//     {
-//         id: 24,
-//         name: "Striped Flutter Sleeve Overlap",
-//         category: "women",
-//         image: p24_img,
-//         new_price: 120,
-//         old_price: 150,
-//     },
-// ]
-// export default all_product;
+// Women Section
+import p20_img from "./women/img1.jpg";
+import p21_img from "./women/img4.jpg";
+import p22_img from "./women/img2.jpg";
+import p23_img from "./women/img3.jpg";
+import p24_img from "./women/img5.jpg";
+import p25_img from "./women/img6.jpg";
+import p26_img from "./women/img7.jpg";
+import p27_img from "./women/img8.jpg";
+import p28_img from "./women/img9.jpg";
+import p29_img from "./women/img10.jpg";
+import p30_img from "./women/img11.jpg";
+import p31_img from "./women/img12.jpg";
+import p32_img from "./women/img13.jpg";
+import p33_img from "./women/img14.jpg";
+import p34_img from "./women/img15.jpg";
+import p35_img from "./women/img16.jpg";
+import p36_img from "./women/img17.jpg";
+
+let all_product = [
+    {
+        id: 1,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "kids",
+        image: p1_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 2,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "kids",
+        image: p2_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "kids",
+        image: p3_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "kids",
+        image: p4_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 5,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p5_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 6,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p6_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 7,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p7_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 8,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p8_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 9,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p9_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 10,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p10_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 11,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p101_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 12,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p11_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 13,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p12_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 14,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p13_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 15,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p14_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 16,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p15_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 17,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p16_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 18,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p17_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 19,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p18_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 20,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "men",
+        image: p19_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 21,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p20_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 22,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p21_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 23,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p22_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 24,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p23_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 25,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p24_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 26,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p25_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 27,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p26_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 28,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p27_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 29,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p28_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 30,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p29_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 31,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p30_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 32,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p31_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 33,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p32_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 34,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p33_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 35,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p34_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 36,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p35_img,
+        new_price: 120,
+        old_price: 150,
+    },
+    {
+        id: 37,
+        name: "Striped Flutter Sleeve Overlap",
+        category: "women",
+        image: p36_img,
+        new_price: 120,
+        old_price: 150,
+    }
+];
+
+export default all_product;
