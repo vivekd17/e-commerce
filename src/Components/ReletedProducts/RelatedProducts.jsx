5a7039afaa0761068d27905ca672e911e46bdd8e
vivekd17 <vivekvalvid17@gmail.com>
2024-03-08 23:35:21 +0530

    new relatedProduct

---
+++ src/Components/ReletedProducts/RelatedProducts.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './RelatedProducts.css'
+import data_product from '../Assets/data'
+import Item from '../Item/Item'
+
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+      <h1>Related Prducts</h1>
+      <hr />
+      <div className="relatedproducts-item">
+        {data_product.map((item,i)=>{
+             return <Item key={i} id={item.id} name={item.name} image={item.image} old_price={item.old_price} new_price={item.new_price}/>   
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
